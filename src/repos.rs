2a6e5a058235a938d514865edd697d5dd5ba1ea8
Christian Helle <christian.helle@outlook.com>
2025-05-26 12:28:16 +0200

    Handle errors and simplify list_repos()

--- src/repos.rs
+++ src/repos.rs
@@ -5,7 +5,8 @@ use azure_devops_rust_api::git::{self, ClientBuilder};
 
 pub async fn handle_command(subcommand: &ReposSubCommands) -> Result<()> {
     // Ensure user is authenticated
-    let credentials = auth::get_credentials()?;    match subcommand {
+    let credentials = auth::get_credentials()?;
+    match subcommand {
         ReposSubCommands::Create { project } => {
             println!("Creating a repository in project: {}", project);
             // Implementation would go here
@@ -15,9 +16,10 @@ pub async fn handle_command(subcommand: &ReposSubCommands) -> Result<()> {
                 "Listing all repos for organization: {}, project: {}",
                 credentials.organization, project
             );
-
             let repos = list_repos(project).await?;
-            println!("{} repos found", repos.len());
+            for repo in repos.iter() {
+                println!("{}", repo.name);
+            }
         }
         ReposSubCommands::Delete { id, project } => {
             println!("Deleting repo with id: {} in project: {}", id, project);
@@ -36,19 +38,19 @@ pub async fn handle_command(subcommand: &ReposSubCommands) -> Result<()> {
 }
 
 async fn list_repos(project: &str) -> Result<Vec<git::models::GitRepository>, anyhow::Error> {
-    let result = auth::get_credentials();
-    let creds = result.unwrap();
-    let organization = creds.organization;
-    let pat = creds.pat;
-    let credential = azure_devops_rust_api::Credential::Pat(pat);
-    let repos = ClientBuilder::new(credential)
-        .build()
-        .repositories_client()
-        .list(organization, project)
-        .await?
-        .value;
-    for repo in repos.iter() {
-        println!("{}", repo.name);
+    match auth::get_credentials() {
+        Ok(creds) => {
+            let credential = azure_devops_rust_api::Credential::Pat(creds.pat);
+            Ok(ClientBuilder::new(credential)
+                .build()
+                .repositories_client()
+                .list(creds.organization, project)
+                .await?
+                .value)
+        }
+        Err(e) => {
+            println!("Unable to retrieve repositories");
+            Err(e)
+        }
     }
-    Ok(repos)
 }
