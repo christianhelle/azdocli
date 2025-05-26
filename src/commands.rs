a442874290022852b09590b42603b5f082fe5a34
Christian Helle <christian.helle@outlook.com>
2025-05-26 23:16:42 +0200

    Remove ReposSubCommands enum and handle_repos_command function from commands module

--- src/commands.rs
+++ src/commands.rs
@@ -27,59 +27,12 @@ pub enum SubCommands {
     },
 }
 
-#[derive(Subcommand, Clone)]
-pub enum ReposSubCommands {
-    /// Create a new repository
-    Create {
-        /// Team project name
-        #[clap(short, long)]
-        project: String,
-    },
-    /// List all repositories
-    List {
-        /// Team project name
-        #[clap(short, long)]
-        project: String,
-    },
-    /// Delete a repository
-    Delete {
-        /// ID of the repository to delete
-        #[clap(short, long)]
-        id: String,
-        /// Team project name
-        #[clap(short, long)]
-        project: String,
-    },
-    /// Show details of a repository
-    Show {
-        /// ID of the repository to show
-        #[clap(short, long)]
-        id: String,
-        /// Team project name
-        #[clap(short, long)]
-        project: String,
-    },
-    /// Update a repository
-    Update {
-        /// ID of the repository to update
-        #[clap(short, long)]
-        id: String,
-        /// Team project name
-        #[clap(short, long)]
-        project: String,
-    },
-}
-
 pub async fn handle_pipelines_command(subcommand: &SubCommands) -> Result<()> {
     crate::pipelines::handle_command(subcommand).await
 }
 
 pub async fn handle_boards_command(subcommand: &SubCommands) -> Result<()> {
     crate::boards::handle_command(subcommand).await
-}
-
-pub async fn handle_repos_command(subcommand: &ReposSubCommands) -> Result<()> {
-    crate::repos::handle_command(subcommand).await
 }
 
 pub async fn handle_artifacts_command(subcommand: &SubCommands) -> Result<()> {
