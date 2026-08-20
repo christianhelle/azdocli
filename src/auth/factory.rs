82ab83c7fede4f582a263d1fdf1bc83defcbb98e
Christian Helle <christian.helle@outlook.com>
2026-08-20 15:36:10 +0200

    Use configured base_url when building Azure DevOps API clients

--- src/auth/factory.rs
+++ src/auth/factory.rs
@@ -6,6 +6,7 @@
 //! logic behind a single **seam** so callers know nothing about
 //! credential resolution.
 
+use crate::auth::url::{normalize_base_url, user_entitlements_base_url};
 use crate::auth::Credentials;
 
 /// Trait for building Azure DevOps API clients.
@@ -38,46 +39,70 @@ pub trait ClientFactory {
 /// A concrete factory backed by a `Credentials` object.
 pub struct CredentialClientFactory {
     credential: azure_devops_rust_api::Credential,
+    endpoint: azure_core::http::Url,
+    entitlements_endpoint: azure_core::http::Url,
 }
 
 impl CredentialClientFactory {
     /// Create a new factory from raw credentials.
     pub fn new(creds: &Credentials) -> Self {
+        let endpoint = normalize_base_url(&creds.base_url)
+            .parse()
+            .expect("base_url should be a valid URL");
+        let entitlements_endpoint = normalize_base_url(&user_entitlements_base_url(&creds.base_url))
+            .parse()
+            .expect("entitlements base_url should be a valid URL");
+
         Self {
             credential: azure_devops_rust_api::Credential::Pat(creds.pat.clone()),
+            endpoint,
+            entitlements_endpoint,
         }
     }
 }
 
 impl ClientFactory for CredentialClientFactory {
     fn build_wit(&self) -> azure_devops_rust_api::wit::Client {
-        azure_devops_rust_api::wit::ClientBuilder::new(self.credential.clone()).build()
+        azure_devops_rust_api::wit::ClientBuilder::new(self.credential.clone())
+            .endpoint(self.endpoint.clone())
+            .build()
     }
 
     fn build_git(&self) -> azure_devops_rust_api::git::Client {
-        azure_devops_rust_api::git::ClientBuilder::new(self.credential.clone()).build()
+        azure_devops_rust_api::git::ClientBuilder::new(self.credential.clone())
+            .endpoint(self.endpoint.clone())
+            .build()
     }
 
     fn build_pipelines(&self) -> azure_devops_rust_api::pipelines::Client {
-        azure_devops_rust_api::pipelines::ClientBuilder::new(self.credential.clone()).build()
+        azure_devops_rust_api::pipelines::ClientBuilder::new(self.credential.clone())
+            .endpoint(self.endpoint.clone())
+            .build()
     }
 
     fn build_core(&self) -> azure_devops_rust_api::core::Client {
-        azure_devops_rust_api::core::ClientBuilder::new(self.credential.clone()).build()
+        azure_devops_rust_api::core::ClientBuilder::new(self.credential.clone())
+            .endpoint(self.endpoint.clone())
+            .build()
     }
 
     fn build_wiki(&self) -> azure_devops_rust_api::wiki::Client {
-        azure_devops_rust_api::wiki::ClientBuilder::new(self.credential.clone()).build()
+        azure_devops_rust_api::wiki::ClientBuilder::new(self.credential.clone())
+            .endpoint(self.endpoint.clone())
+            .build()
     }
 
     fn build_search(&self) -> azure_devops_rust_api::search::Client {
-        azure_devops_rust_api::search::ClientBuilder::new(self.credential.clone()).build()
+        azure_devops_rust_api::search::ClientBuilder::new(self.credential.clone())
+            .endpoint(self.endpoint.clone())
+            .build()
     }
 
     fn build_entitlements(&self) -> azure_devops_rust_api::member_entitlement_management::Client {
         azure_devops_rust_api::member_entitlement_management::ClientBuilder::new(
             self.credential.clone(),
         )
+        .endpoint(self.entitlements_endpoint.clone())
         .build()
     }
 }
